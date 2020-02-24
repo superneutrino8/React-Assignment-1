f5aabbd1b8cf734361f527548c7b1cf6a2acffca
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-02-24 21:45:35 +0530

    Added Inital Components

---
+++ src/Components/UnserInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UnserInput extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+            </div>
+        );
+    }
+}
+
+export default UnserInput;
